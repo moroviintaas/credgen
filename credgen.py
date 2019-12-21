0adc77b34a0215da121895666145caba2049dfdd
moroviintaas <robertexcracovia@gmail.com>
2019-12-21 16:08:34 +0100

    Created guided mode.

--- credgen.py
+++ credgen.py
@@ -4,16 +4,21 @@ import hashlib
 import argparse
 
 DEFAULT_SALT_LENGTH = 8
+DEFAULT_LINE_LENGTH = 8
 
 def none2empty(array):
     return [x if x != None else "" for x in array] 
 
-def generate_password(passphrase, username=None, resourcename=None, salt=None, saltlength = DEFAULT_SALT_LENGTH):
+
+
+    
+
+def generate_password(passphrase, username=None, resourcename=None, salt=None, salt_length = DEFAULT_SALT_LENGTH):
 
     #(passphrase, username, resourcename, salt) = none2empty((passphrase, username, resourcename, salt))
     
 
-    
+    #print (passphrase, username, resourcename, salt, salt_length)
 
     hctx = hashlib.sha256()
     hctx.update(passphrase.encode('utf-8'))
@@ -26,10 +31,12 @@ def generate_password(passphrase, username=None, resourcename=None, salt=None, s
     #print(salt)
     if salt != None:
         si = int(salt, 16)
-        salt_b = si.to_bytes(saltlength, byteorder = "big")
+        print(type(si),si)
+        salt_b = si.to_bytes(salt_length, byteorder = "big")
         hctx.update(salt_b)
         #print(salt_b)
 
+    
     h = hctx.digest()
     return h
 
@@ -51,6 +58,33 @@ def result2str(r, signs_in_row = None, altchars='!.'):
   #      print (rows)
   #      print (type(rows[0]))
         return "".join(str(x)  +"\n" for x in rows)
+
+def ask_params():
+    username = input("Your username:\t\t\t")
+
+    resourcename = input("Resource name (server):\t\t")
+
+    passphrase = input("Passphrase (may be a line):\t")
+
+    salt = input("Salt:\t\t\t\t")
+
+    if len(salt)%2 == 1:
+        salt = "0" + salt
+    salt_length = len(salt)//2 -1
+
+    if username == "":
+        username = None
+    if resourcename == "":
+        resourcename = None
+    if salt == "":
+        salt = None
+
+    
+
+    dgst = generate_password(passphrase, username, resourcename,\
+                               salt, salt_length)
+    result = result2str(dgst, DEFAULT_LINE_LENGTH,  ".!")
+    return result
         
 
 if __name__ == "__main__":
@@ -68,16 +102,20 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--salt", help="Specify salt in hex e.g 0x01020304 (in big endian)",
                     dest="salt", default = None)
     parser.add_argument("-a", "--altchars", help="Specify alternative chars for '+' and '/'",
-                    dest="altchars", default = None)
+                    dest="altchars", default = ".!")
     parser.add_argument("-l", "--linelength", help="Specify the number of characters in row",
-                    dest="linelength", default = None, type=int)
+                    dest="linelength", default = DEFAULT_LINE_LENGTH, type=int)
     parser.add_argument("-S", "--saltlength", help="Specify the number of bytes in salt",
                     dest="saltlength", default = DEFAULT_SALT_LENGTH, type=int)
 
     args=parser.parse_args()
     #print(args.passphrase)
-    
-    dgst = generate_password(args.passphrase, args.username, args.resourcename,\
+    if args.passphrase == None and args.username == None and\
+       args.resourcename == None and args.salt == None:
+        print(ask_params())
+    else:
+        
+        dgst = generate_password(args.passphrase, args.username, args.resourcename,\
                                args.salt, args.saltlength)
-    result = result2str(dgst, args.saltlength,  args.altchars)
-    print(result)
+        result = result2str(dgst, args.linelength,  args.altchars)
+        print(result)
